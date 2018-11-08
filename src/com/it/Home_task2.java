554a95a058da98c93d17026619d48f60cc586422
Oleg Zubko <oleg.zubko@globallogic.com>
2018-11-08 18:08:52 +0200

    commit 2

--- src/com/it/Home_task2.java
+++ src/com/it/Home_task2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Home_task2 {
     public static void main(String[] args) {
 
-        //Task_2.1
+        //Task_2.1, Task_2.2
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter quantity of numbers: ");
         Integer n = scan.nextInt();
@@ -16,6 +16,7 @@ public class Home_task2 {
         String next= "";
         String shortest = "";
         String longest = "";
+        String lmed[] =new  "";
 
 
         System.out.print("Please enter " + n + " whole numbers: ");
@@ -41,7 +42,25 @@ public class Home_task2 {
         shortest = num[0];
         longest = num[n - 1];
         System.out.println(Arrays.toString(num));
-        System.out.println("The shortest number: "+ shortest + " Quantity of digits: " + shortest.length());
-        System.out.println("The longest number: "+ longest + " Quantity of digits: " + longest.length());
+        System.out.println("The shortest number: "+ shortest + ", Quantity of digits: " + shortest.length());
+        System.out.println("The longest number: "+ longest + ", Quantity of digits: " + longest.length());
+
+        //Task_2.3
+        for (int i = n-1; i > 0; i--) {
+            for (int i1 = 0; i1 < i; i1++) {
+                previous = num[i1];
+                next = num[i1 + 1];
+                if (previous.length() > next.length()) {
+                    String temp = num[i1];
+                    num[i1] = num[i1 + 1];
+                    num[i1 + 1] = temp;
+                }
+            }
+        }
+        shortest = num[0];
+        longest = num[n - 1];
+        System.out.println(Arrays.toString(num));
+        System.out.println("The shortest number: "+ shortest + ", Quantity of digits: " + shortest.length());
+        System.out.println("The longest number: "+ longest + ", Quantity of digits: " + longest.length());
     }
 }
