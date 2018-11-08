5fe6180222c83a8803cf53868da2a26b6c4d0396
Oleg Zubko <olegzubko82@gmail.com>
2018-11-08 23:42:38 +0200

    Add Task_2.3

--- src/com/it/Home_task2.java
+++ src/com/it/Home_task2.java
@@ -6,29 +6,24 @@ import java.util.Scanner;
 public class Home_task2 {
     public static void main(String[] args) {
 
-        //Task_2.1, Task_2.2
+        //Task_2
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter quantity of numbers: ");
         Integer n = scan.nextInt();
 
-        //int intnum[] = new int[n];
         String previous = "";
-        String next= "";
+        String next = "";
         String shortest = "";
         String longest = "";
-        String lmed[] =new  "";
-
+        int medlength = 0;
 
         System.out.print("Please enter " + n + " whole numbers: ");
         Scanner scan1 = new Scanner(System.in);
         String vvod = scan1.nextLine();
         String[] num = vvod.split(" ");
 
-        /*for (int i = 0; i < n; i++) {
-            intnum[i] = Integer.parseInt(num[i]);
-        }*/
-
-        for (int i = n-1; i > 0; i--) {
+        //Task_2.1, Task_2.2
+        /*for (int i = n-1; i > 0; i--) {
             for (int i1 = 0; i1 < i; i1++) {
                 previous = num[i1];
                 next = num[i1 + 1];
@@ -43,24 +38,28 @@ public class Home_task2 {
         longest = num[n - 1];
         System.out.println(Arrays.toString(num));
         System.out.println("The shortest number: "+ shortest + ", Quantity of digits: " + shortest.length());
-        System.out.println("The longest number: "+ longest + ", Quantity of digits: " + longest.length());
+        System.out.println("The longest number: "+ longest + ", Quantity of digits: " + longest.length());*/
 
         //Task_2.3
-        for (int i = n-1; i > 0; i--) {
-            for (int i1 = 0; i1 < i; i1++) {
-                previous = num[i1];
-                next = num[i1 + 1];
-                if (previous.length() > next.length()) {
-                    String temp = num[i1];
-                    num[i1] = num[i1 + 1];
-                    num[i1 + 1] = temp;
-                }
+        /*for (int i = 0; i < n; i++) {
+            medlength += num[i].length();
+        }
+        System.out.println("Medium length = "+ (medlength = medlength / n));
+        System.out.println();
+        System.out.println("Numbers with length less than medium length:");
+        for (int i = 0; i < n; i++) {
+            if (num[i].length() < medlength) {
+                System.out.println("Number: " + num[i] + ", Length: " + num[i].length());
             }
         }
-        shortest = num[0];
-        longest = num[n - 1];
-        System.out.println(Arrays.toString(num));
-        System.out.println("The shortest number: "+ shortest + ", Quantity of digits: " + shortest.length());
-        System.out.println("The longest number: "+ longest + ", Quantity of digits: " + longest.length());
+        System.out.println();
+        System.out.println("Numbers with length more than medium length:");
+        for (int i = 0; i < n; i++) {
+            if (num[i].length() > medlength) {
+                System.out.println("Number: " + num[i] + ", Length: " + num[i].length());
+            }
+        }*/
+
+
     }
 }
